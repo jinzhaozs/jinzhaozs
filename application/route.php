e12085effd94a4547c369d4ff50c6fcad5115279
15930167221 <15930167221@163.com>
2018-04-09 11:40:53 +0800

    yxy

---
+++ application/route.php
@@ -0,0 +1,19 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+use think\Route;
+// 注册路由到index模块的News控制器的read操作
+Route::rule('/','index/Index/index');//查询
+Route::rule('/index/Index/add','index/Index/add');//添加
+Route::post('/index/Index/doadd','index/Index/doadd');//执行添加
+Route::get('/index/Index/edit/:id','index/Index/edit');//修改
+Route::post('/index/Index/doedit','index/Index/doedit');//执行修改
+Route::get('/index/Index/del/:id','index/Index/del');//删除
