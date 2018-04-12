8a1a59c3b57d8418d947799f2561e4bd8e9b8a43
15930167221 <15930167221@163.com>
2018-04-12 09:51:01 +0800

    yxy

--- application/database.php
+++ application/database.php
@@ -13,13 +13,21 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '192.168.12.142',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'jzzs',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root',
+    'password'        => '',
+     // 服务器地址
+    // 'hostname'        => '192.168.12.142',
+    // // 数据库名
+    // 'database'        => 'jzzs',
+    // // 用户名
+    // 'username'        => 'root',
+    // // 密码
+    // 'password'        => 'root',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
