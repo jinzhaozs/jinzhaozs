e987fb4e866d7d7db6dbff7b93e187d0b435f26e
2390668124 <2390668124@qq.com>
2018-04-26 16:18:16 +0800

    cjj

--- application/route.php
+++ application/route.php
@@ -150,7 +150,7 @@ Route::rule('/admin/photo/index','admin/photo/index');//户型风格
 Route::rule('/admin/photo/add','admin/photo/add');// 添加
 Route::rule('/admin/photo/edit','admin/photo/edit');// 修改
 Route::rule('/admin/photo/del','admin/photo/del');// 删除
-//员工
+//人员
 Route::rule('/admin/user/index','admin/user/index');//员工详情
 Route::rule('/admin/user/add','admin/user/add');//添加
 Route::rule('/admin/user/edit','admin/user/edit');//修改
@@ -158,6 +158,14 @@ Route::rule('/admin/user/delete','admin/user/delete');//删除
 Route::rule('/admin/user/ajaxcode','admin/user/ajaxcode');//账号验证
 Route::rule('/admin/user/xg','admin/user/xg');//修改密码
 Route::rule('/admin/user/ajaxyuan','admin/user/ajaxyuan');//判断原密码
+//员工
+Route::rule('/admin/ruser/index','admin/ruser/index');//员工详情
+Route::rule('/admin/ruser/add','admin/ruser/add');//添加
+Route::rule('/admin/ruser/edit','admin/ruser/edit');//修改
+Route::rule('/admin/ruser/delete','admin/ruser/delete');//删除
+Route::rule('/admin/ruser/ajaxcode','admin/ruser/ajaxcode');//账号验证
+Route::rule('/admin/ruser/xg','admin/ruser/xg');//修改密码
+Route::rule('/admin/ruser/ajaxyuan','admin/ruser/ajaxyuan');//判断原密码
 //部门
 Route::rule('/admin/bm/index','admin/bm/index');//部门
 Route::rule('/admin/bm/add','admin/bm/add');// 添加
